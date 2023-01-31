71432a6e87ebbae6bfdaac0984bdf097c926e64a
Jay <roebuck.jayc@gmail.com>
2023-01-31 10:04:36 -0500

    removed useless backend

--- backend/routes/tests/sampleHelper.test.ts
+++
@@ -1,8 +0,0 @@
-import myFunction from "../sampleHelper";
-
-describe('Unit test on helper function', () => {
-    it('tests the only function', () => {
-        const value = myFunction(3, 6)
-        expect(value).toBe(9);
-    });
-});
