0781d114deb1fb8a9d7fe38e9cc7ad0d141f9864
Jay <roebuck.jayc@gmail.com>
2023-02-01 15:46:23 -0500

    prepared testing on Next (NavBar is sample)

--- components/NavBar/tests/Navbar.test.tsx
+++ components/NavBar/tests/Navbar.test.tsx
@@ -1,13 +1,54 @@
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { useAuth } from '@/contexts/AuthContext';
+import '@testing-library/jest-dom'
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import NavBar from '../NavBar';
 
-it('render navBar component', async () => {
+jest.mock("contexts/AuthContext", () => ({
+    useAuth: jest.fn()
+}))
+
+const mockedUseAuth = useAuth as jest.Mock<any>;
+
+it('render nav bar component with a user', async () => {
+    mockedUseAuth.mockImplementation(() => {
+        return {
+            currentUser: {}
+        }
+    })
     const { findByText } = render(
-        <MemoryRouter>
             <NavBar />
-        </MemoryRouter>
     );
-    const navbarElement = await findByText("Interlinked", { exact: false});
+    const navbarElement = await findByText("Logout", { exact: false });
+    expect(navbarElement).toBeInTheDocument();
+});
+
+it('calls the logout function on button click', async () => {
+    const logout = jest.fn()
+    mockedUseAuth.mockImplementation(() => {
+        return {
+            currentUser: {},
+            logout: logout
+        }
+    })
+    const { findByText } = render(
+            <NavBar />
+    );
+    const logoutButton = await findByText("Logout", { exact: false });
+
+    fireEvent.click(logoutButton)
+    await waitFor(() => {expect(logout).toBeCalled()})
+});
+
+it('render nav bar component without a user', async () => {
+    mockedUseAuth.mockImplementation(() => {
+        return {
+            currentUser: null
+        }
+    })
+
+    const { findByText } = render(
+            <NavBar />
+    );
+    const navbarElement = await findByText("Register", { exact: false });
     expect(navbarElement).toBeInTheDocument();
 });
