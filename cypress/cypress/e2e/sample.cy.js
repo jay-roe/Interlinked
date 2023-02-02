45f3932e071c9e7563dd6a81cb84f9d0260f2d82
Jay <roebuck.jayc@gmail.com>
2023-02-01 23:12:53 -0500

    next + ci

--- cypress/cypress/e2e/sample.cy.js
+++ cypress/cypress/e2e/sample.cy.js
@@ -3,11 +3,4 @@ describe('Sample spec', () => {
     cy.visit('')  // Visits the baseUrl by default
     cy.get('[data-testid="home-brand"]').should('have.text', 'Interlinked')
   })
-
-  // As you can see, if the front or backend isn't running, this test will fail. TRUE end-to-end action baby
-  it('gets data from the backend', () => {
-    cy.visit('cypressDemo')
-    cy.get('[data-testid="demo-button"]').click()
-    cy.get('[data-testid="demo-component"]').should('contain', "route of the backend")
-  })
 })
