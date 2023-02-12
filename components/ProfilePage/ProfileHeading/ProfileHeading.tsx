5b774a6608d5084c0586bb4ea0f4f8bd093159a2
Adamo Orsini <adamo.orsini01@gmail.com>
2023-02-12 18:05:54 -0500

    Vertically center small text when editing

--- components/ProfilePage/ProfileHeading/ProfileHeading.tsx
+++ components/ProfilePage/ProfileHeading/ProfileHeading.tsx
@@ -41,7 +41,7 @@ export default function ProfileHeading({
         <h1 data-testid="profile-title" className="text-3xl font-extrabold">
           {currentUser?.name || 'No name provided.'}
         </h1>
-        <div className="flex">
+        <div className="flex items-center">
           {bioEditing ? (
             <textarea
               className="mb-2 mt-2 block min-h-[75px] w-full appearance-none rounded border border-gray-300 bg-gray-50 px-3 py-2 text-sm text-gray-900 placeholder-gray-500 focus:z-10 focus:border-blue-500 focus:outline-none focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
