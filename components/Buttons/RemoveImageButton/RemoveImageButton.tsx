197fa17852422be72537a79ec9cb5c3002592552
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-02-15 02:55:22 -0500

    create button for removing image

---
+++ components/Buttons/RemoveImageButton/RemoveImageButton.tsx
@@ -0,0 +1,7 @@
+export default function RemoveImageButton() {
+  return (
+    <div className="    flex items-center gap-2 rounded-md bg-accent-orange p-1 px-2  font-para-med font-bold text-black hover:text-white ">
+      Remove
+    </div>
+  );
+}
