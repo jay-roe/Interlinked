06be62841ba8c37199cbed21378746e429356d11
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-02-15 02:55:49 -0500

    created  button for uploading media

--- components/Buttons/UploadMediaButton/UploadMediaButton.tsx
+++ components/Buttons/UploadMediaButton/UploadMediaButton.tsx
@@ -1,1 +1,10 @@
-export {};
+import { FaCloudUploadAlt } from 'react-icons/fa';
+
+export default function UploadMediaButton() {
+  return (
+    <div className="    flex items-center gap-2 rounded-md bg-accent-orange p-1 px-2  font-para-med font-bold text-black hover:text-white ">
+      Upload Media
+      <FaCloudUploadAlt />
+    </div>
+  );
+}
