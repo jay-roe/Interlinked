b9ed3cc49220703b5d4aec8352cdfb9d210cdd6a
Jay <roebuck.jayc+github@gmail.com>
2023-02-17 18:34:16 -0500

    added profile project tests

--- components/ProfilePage/ProfileProjects/tests/ProfileProjects.test.tsx
+++ components/ProfilePage/ProfileProjects/tests/ProfileProjects.test.tsx
@@ -1,6 +1,13 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { Timestamp } from 'firebase/firestore';
 import ProfileProjects from '../ProfileProjects';
+
+let mockDate = {
+  toDate: () => {
+    return new Date();
+  },
+} as unknown as Timestamp;
 
 const currentUser = {
   awards: [],
@@ -12,15 +19,27 @@ const currentUser = {
   experience: [],
   languages: [],
   name: '',
-  projects: [],
+  projects: [
+    {
+      title: 'not losing my mind',
+      collaborators: [],
+      startDate: mockDate,
+    },
+    {
+      title: 'auto puzzle',
+      collaborators: [],
+      startDate: mockDate,
+      endDate: mockDate,
+    },
+  ],
   recommendations: [],
   skills: [],
   volunteering: [],
 };
 
 it('renders projects given user', async () => {
-//   const { findByText } = render(<ProfileProjects currentUser={currentUser} />);
+  const { findByText } = render(<ProfileProjects currentUser={currentUser} />);
 
-//   const projectName = await findByText('eating', { exact: false });
-//   expect(projectName).toBeInTheDocument();
+  const projectName = await findByText('not losing my mind', { exact: false });
+  expect(projectName).toBeInTheDocument();
 });
