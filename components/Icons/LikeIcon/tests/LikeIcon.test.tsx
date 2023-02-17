1a5adcfd847299eb4819df1c225a37c273cc65ab
Jay <roebuck.jayc+github@gmail.com>
2023-02-16 20:21:04 -0500

    added like icon tests

---
+++ components/Icons/LikeIcon/tests/LikeIcon.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/react';
+import LikeIcon from '../LikeIcon';
+
+it('renders dislike icon by default', async () => {
+  const { findByText, findByTestId } = render(<LikeIcon likes={5} />);
+
+  const likeCount = await findByText('5', { exact: false });
+  const dislikeIcon = await findByTestId('dislike');
+
+  expect(likeCount).toBeInTheDocument();
+  expect(dislikeIcon).toBeInTheDocument();
+});
+
+it('renders like icon', async () => {
+  const { findByTestId } = render(<LikeIcon likes={5} />);
+
+  const likeButton = await findByTestId('like-btn');
+  fireEvent.click(likeButton);
+
+  const likeIcon = await findByTestId('like');
+  expect(likeIcon).toBeInTheDocument();
+});
