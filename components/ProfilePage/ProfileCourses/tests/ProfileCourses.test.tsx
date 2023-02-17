2cad6a9415dbfd0998775cd89913f1bae0dc5bf2
Jay <roebuck.jayc+github@gmail.com>
2023-02-16 19:57:04 -0500

    added profile courses tests

---
+++ components/ProfilePage/ProfileCourses/tests/ProfileCourses.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import ProfileCourses from '../ProfileCourses';
+
+const currentUser = {
+  awards: [],
+  codingLanguages: [],
+  connections: [],
+  courses: [
+    { title: 'intro to testing' },
+    { title: "why you shouldn't go into civil" },
+    { title: 'blackmail 201' },
+  ],
+  education: [],
+  email: '',
+  experience: [],
+  languages: [],
+  name: '',
+  projects: [],
+  recommendations: [],
+  skills: [],
+  volunteering: [],
+};
+
+it('renders courses given user', async () => {
+  const { findByText } = render(<ProfileCourses currentUser={currentUser} />);
+
+  const courseTitle = await findByText('intro to testing', { exact: false });
+  expect(courseTitle).toBeInTheDocument();
+});
