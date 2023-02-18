105fdb33177d9087a7dac45f7cc055152a6c9566
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-02-18 12:33:49 -0500

    LinkButtonNoNumber test

--- components/Buttons/LinkButton/tests/LinkButtonNoNumber.test.tsx
+++ components/Buttons/LinkButton/tests/LinkButtonNoNumber.test.tsx
@@ -1,3 +1,4 @@
+import LinkIcon from '@/components/Icons/LinkIcon/LinkIcon';
 import '@testing-library/jest-dom';
 import { fireEvent, render } from '@testing-library/react';
 import LinkButtonNoNumber from '../LinkButtonNoNumber';
@@ -8,4 +9,18 @@ it('renders link icon no number when clicked', async () => {
   const linkButtonNoNumber = await findByTestId('link-btn-no-number');
   fireEvent.click(linkButtonNoNumber);
   expect(linkButtonNoNumber).toBeInTheDocument();
+});
+
+it('renders link icon small', async () => {
+  const { findByTestId } = render(<LinkButtonNoNumber small={false} />);
+
+  const linkIcon = await findByTestId('link-btn');
+  expect(linkIcon).toBeInTheDocument();
+});
+
+it('renders link icon', async () => {
+  const { findByTestId } = render(<LinkButtonNoNumber small={true} />);
+
+  const linkIcon = await findByTestId('link-btn');
+  expect(linkIcon).toBeInTheDocument();
 });
