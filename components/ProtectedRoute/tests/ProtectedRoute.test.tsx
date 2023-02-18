82198b199b66ea09aa3d35c88f11f9c927f04ec1
Jay <roebuck.jayc+github@gmail.com>
2023-02-17 19:55:33 -0500

    fixed protected route test

--- components/ProtectedRoute/tests/ProtectedRoute.test.tsx
+++ components/ProtectedRoute/tests/ProtectedRoute.test.tsx
@@ -2,87 +2,69 @@
 // it checks if the user is logged in and rediects them to the Login page if they aren't
 
 import { useAuth } from '@/contexts/AuthContext';
-import '@testing-library/jest-dom'
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import { useRouter } from 'next/navigation';
-import { ProtectedRoute } from "../ProtectedRoute";
-
+import { ProtectedRoute } from '../ProtectedRoute';
 
 // mock authentication and navigation modules:
 
-jest.mock('contexts/AuthContext', () => ({ 
-    useAuth: jest.fn()  // create mock function
-})); 
+jest.mock('contexts/AuthContext', () => ({
+  useAuth: jest.fn(), // create mock function
+}));
 
 jest.mock('next/navigation', () => ({
-    useRouter: jest.fn()    // create mock function
-}))
+  useRouter: jest.fn(), // create mock function
+}));
 
-const mockedUseAuth = useAuth as jest.Mock<any>;  // make useAuth modifiable based on the test case (I wanna mock a logged in and logged out user)
+const mockedUseAuth = useAuth as jest.Mock<any>; // make useAuth modifiable based on the test case (I wanna mock a logged in and logged out user)
 const mockedRouter = useRouter as jest.Mock<any>;
 
 // mock alert function
 jest.spyOn(window, 'alert').mockImplementation(() => {});
 
+it('page is redirected to login when user is not authenticated', async () => {
+  // user is logged out
+  mockedUseAuth.mockImplementation(() => {
+    return {
+      currentUser: null,
+    };
+  });
 
-// this test fails because it says push is not a function in Protected Router. I think it might've
-// been implemented wrong there
-// it("page is redirected to login when user is not authenticated", async () => {
+  // we don't actually want to switch pages. Mock push function so that we can see if the page switched
+  const myPush = jest.fn();
+  mockedRouter.mockImplementation(() => {
+    return {
+      push: myPush,
+    };
+  });
 
-//     // user is logged out
-//     mockedUseAuth.mockImplementation(() => {
-//         return { 
-//             currentUser: null
-//         } 
-//     })
+  // render the protected file
+  render(<ProtectedRoute children={undefined} />);
 
-//     // we don't actually want to switch pages. Mock push function so that we can see if the page switched
-//     const myPush = jest.fn()
-//     mockedRouter.mockImplementation(() => {
-//         return {
-//             push: myPush()
-//         }
-//     })
-//    
-//    render the protected file
-//    render(
-//        <ProtectedRoute children={undefined} />
-//      );
-//
-//
-//     // Check if the router function was called (ie, user was redirected)
-//     expect(myPush).toHaveBeenCalledWith('/login');  
-
-    
-// });
-
-
-// check if the user is authenticated. If they are, there should be no redirect called
-it("page is not redirected to when user is authenticated", async () => {
-
-    // user is logged in
-    mockedUseAuth.mockImplementation(() => {
-        return { 
-            currentUser: { }
-        } 
-    })
-
-    // Mock push function so that we can see if the page switched
-    const myPush = jest.fn()
-    mockedRouter.mockImplementation(() => {
-        return {
-            push: myPush()
-        }
-    })
-   
-   render(
-       <ProtectedRoute children={undefined} />
-     );
-
-
-    // Check if the router function was not called (ie, user was redirected)
-    expect(myPush).not.toHaveBeenCalledWith('/login');  
-
-    
+  // Check if the router function was called (ie, user was redirected)
+  expect(myPush).toHaveBeenCalledWith('/login');
 });
 
+// check if the user is authenticated. If they are, there should be no redirect called
+it('page is not redirected to when user is authenticated', async () => {
+  // user is logged in
+  mockedUseAuth.mockImplementation(() => {
+    return {
+      currentUser: {},
+    };
+  });
+
+  // Mock push function so that we can see if the page switched
+  const myPush = jest.fn();
+  mockedRouter.mockImplementation(() => {
+    return {
+      push: myPush(),
+    };
+  });
+
+  render(<ProtectedRoute children={undefined} />);
+
+  // Check if the router function was not called (ie, user was redirected)
+  expect(myPush).not.toHaveBeenCalledWith('/login');
+});
