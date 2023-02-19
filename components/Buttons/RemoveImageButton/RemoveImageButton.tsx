0d5f97b4cef1c0d6c3c50099d6f2562855a01c0a
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-02-18 20:40:47 -0500

    RemoveImageButton test

--- components/Buttons/RemoveImageButton/RemoveImageButton.tsx
+++ components/Buttons/RemoveImageButton/RemoveImageButton.tsx
@@ -1,6 +1,9 @@
 export default function RemoveImageButton() {
   return (
-    <div className="    flex items-center gap-2 rounded-md bg-accent-orange p-1 px-2  font-para-med font-bold text-black hover:text-white ">
+    <div
+      data-testid={'remove-image-button'}
+      className="flex items-center gap-2 rounded-md bg-accent-orange p-1 px-2  font-para-med font-bold text-black hover:text-white "
+    >
       Remove
     </div>
   );
