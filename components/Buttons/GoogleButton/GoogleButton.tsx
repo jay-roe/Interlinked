fadebcf6eac26670d3e8ae1d0a8c7ff9cfba60a2
Adamo Orsini <adamo.orsini01@gmail.com>
2023-02-23 14:42:49 -0500

    Added post with multiple images

--- components/Buttons/GoogleButton/GoogleButton.tsx
+++ components/Buttons/GoogleButton/GoogleButton.tsx
@@ -1,9 +1,11 @@
+import type { ComponentProps } from 'react';
 import { FaGoogle } from 'react-icons/fa';
 
-export default function GoogleButton(props) {
+export default function GoogleButton(props: ComponentProps<'button'>) {
   return (
     <button
       {...props}
+      type="button"
       className={`${props.className} mr-2 mb-2 inline-flex items-center rounded-lg bg-sky-800 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-500 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900  `}
     >
       <FaGoogle className="mr-2" />
