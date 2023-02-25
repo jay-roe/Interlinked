eaa18885f65f852b8bba2bbf401bb1e22ed12076
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2023-02-25 12:36:13 -0500

    notif page prep

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -105,6 +105,7 @@ export default function NavBar() {
               </div>
               {currentUser && (
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  {/* Notification button */}
                   <button
                     type="button"
                     className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
