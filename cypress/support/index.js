afd308c4e8308b28ecaf20105581d1ec46825d12
Jay <roebuck.jayc+github@gmail.com>
2023-03-10 18:12:05 -0500

    Revert "Merge branch 'main' into 123-testing-revamp"
    This reverts commit 01ec4a7e2517d8819c1fbd6cea0907cb8bccf6f6.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,14 @@
 Cypress.Screenshot.defaults({
   screenshotOnRunFailure: false,
 });
+
+/*
+Cypress.on('test:before:run', () => {
+  Cypress.automation('remote:debugger:protocol', {
+    command: 'Emulation.setLocaleOverride',
+    params: {
+      locale: 'en-GB',
+    },
+  });
+});
+*/
