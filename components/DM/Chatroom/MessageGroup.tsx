22b4998715c1f84fe0d5146b2712c77a107afd6f
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-09 20:10:04 -0500

    removed messageGroup

--- components/DM/Chatroom/MessageGroup.tsx
+++
@@ -1,9 +0,0 @@
-import Card from '../../Card/Card';
-import Message from './Message';
-import { MessageInterface } from '@/types/Message';
-
-const MessageGroup = ({ message }: { message: MessageInterface }) => {
-  return <Message message={message} />;
-};
-
-export default MessageGroup;
