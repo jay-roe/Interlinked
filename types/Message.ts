0cb5300ebf8ab2533dea31b622c2377270d3a1e5
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-09 23:50:42 -0500

    Created ChatMessage type in order to encode id

--- types/Message.ts
+++ types/Message.ts
@@ -3,6 +3,11 @@ import type { Timestamp } from 'firebase/firestore';
 
 export type Message = {
   content: string;
-  sender?: User;
+  sender: { name: string; profilePicture: string };
   time_stamp?: Timestamp;
+};
+
+export type ChatMessage = {
+  message: Message;
+  id: string;
 };
