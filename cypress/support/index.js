aa31da0437fafc38ec7e8844641d2b615a6f33f3
Jay <roebuck.jayc+github@gmail.com>
2023-03-11 13:53:59 -0500

    remove rather chunky comment blocks

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,13 +2,3 @@ Cypress.Screenshot.defaults({
   screenshotOnRunFailure: false,
 });
 
-/*
-Cypress.on('test:before:run', () => {
-  Cypress.automation('remote:debugger:protocol', {
-    command: 'Emulation.setLocaleOverride',
-    params: {
-      locale: 'en-GB',
-    },
-  });
-});
-*/
