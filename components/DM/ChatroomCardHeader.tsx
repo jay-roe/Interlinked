3c935bb9273a2a184fa073f31445ea5669840a06
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-11 04:20:07 -0500

    added user prop

--- components/DM/ChatroomCardHeader.tsx
+++ components/DM/ChatroomCardHeader.tsx
@@ -1,20 +1,26 @@
-import { Message } from '@/types/Message';
-import { RiCheckboxBlankCircleFill } from 'react-icons/ri';
+import { ChatRoom, Message } from '@/types/Message';
+import { User } from '@/types/User';
 
-export default function notificationHeader({ message }: { message: Message }) {
+export default function notificationHeader({
+  room,
+  user,
+}: {
+  room: ChatRoom;
+  user: User;
+}) {
   return (
     <div className="start flex">
       <img
         className="h-25 min-h-[2rem] w-8 min-w-[2rem] rounded-full p-2 md:h-12 md:w-12"
         src={
-          message.sender.profilePicture ||
+          user.profilePicture ||
           'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
         }
       ></img>
       <div>
-        <p>{message.sender.name}</p>
+        <p>{user.name}</p>
         <p>
-          {message.time_stamp?.toDate().toLocaleString('en-US', {
+          {room.lastMessage.time_stamp?.toDate().toLocaleString('en-US', {
             month: 'long',
             year: 'numeric',
             day: '2-digit',
