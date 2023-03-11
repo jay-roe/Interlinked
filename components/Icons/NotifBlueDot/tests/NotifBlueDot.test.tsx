86f915812bc5b5c1db022953a9720b7346a395e2
melisa1026 <melipan01@gmail.com>
2023-03-10 23:08:21 -0500

    test if user is not logged in on notification page

--- components/Icons/NotifBlueDot/tests/NotifBlueDot.test.tsx
+++ components/Icons/NotifBlueDot/tests/NotifBlueDot.test.tsx
@@ -2,14 +2,13 @@ import '@testing-library/jest-dom';
 import { Timestamp } from 'firebase/firestore';
 import NotifBlueDot from '../NotifBlueDot';
 import { render } from '@testing-library/react';
+import { NotifType } from '@/types/Notification';
 
 it('renders the blue dot', async () => {
   const { findByTestId } = render(
     <NotifBlueDot
       notification={{
-        notifType:
-          'c:/Users/melis/OneDrive/Documents/GitHub/Interlinked_SOEN390_Team11/types/Notification'
-            .POST,
+        notifType: NotifType.POST,
         context: '',
         sender: '',
         notifTime: Timestamp.now(),
