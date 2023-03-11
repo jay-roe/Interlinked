21a07a5cb6646ae87837330c028c9bb6f7525c17
Jay <roebuck.jayc+github@gmail.com>
2023-03-11 11:43:38 -0500

    added assertions for consistency

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -11,14 +11,18 @@ describe('Full navbar spec', () => {
     cy.get('[data-testid=nav-menu]').click();
     cy.get('[data-testid=nav-menu-profile]').click();
     cy.url().should('contain', '/profile');
+    cy.get('[data-testid=profile-title]').should('exist');
 
     cy.get('[data-testid=nav-menu]').click();
     cy.get('[data-testid=nav-menu-edit-profile]').click();
     cy.url().should('contain', '/edit-profile');
+    cy.get('[data-testid=name-edit-button]').should('exist');
 
+    /* reintroduce when we actually go to settings page
     cy.get('[data-testid=nav-menu]').click();
-    cy.get('[data-testid=nav-menu-settings]').click();
+    cy.get('[data-testid=nav-menu-settings]').click();*/
 
+    cy.visit('');
     cy.get('[data-testid=nav-menu]').click();
     cy.get('[data-testid=nav-logout]').click();
     cy.get('[data-testid=base-msg]').should('exist');
