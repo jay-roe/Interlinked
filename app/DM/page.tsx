94778e9e735830065f819cdac6fbfa25d92cc403
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-11 04:18:09 -0500

    created DM page

--- app/DM/page.tsx
+++ app/DM/page.tsx
@@ -1,132 +1,49 @@
 'use client';
-import MessageCard from '@/components/DM/MessageCard';
-import { ChatMessage, Message, ChatRoom } from '@/types/Message';
-import { User, testUser } from '@/types/User';
+import { KeyedChatRoom } from '@/types/Message';
 import { useAuth } from '@/contexts/AuthContext';
-import TimeDivider from '@/components/DM/TimeDivider';
-import MessageButton from '@/components/MessageModal/MessageModal';
-import {
-  doc,
-  getDoc,
-  addDoc,
-  where,
-  serverTimestamp,
-  onSnapshot,
-  query,
-  orderBy,
-  Timestamp,
-  collection,
-  setDoc,
-  updateDoc,
-  collectionGroup,
-  getDocs,
-} from 'firebase/firestore';
+import { where, onSnapshot, query } from 'firebase/firestore';
 
-import { db, typeCollection } from '@/config/firestore';
+import { db } from '@/config/firestore';
 import { useEffect, useState } from 'react';
-import { FaRegPaperPlane } from 'react-icons/fa';
-import { firestore } from '@/config/firebase';
-import CreateChatModal from '@/components/DM/CreateChatModal';
-import MessageModal from '@/components/MessageModal/MessageModal';
+import ChatroomCard from '@/components/DM/ChatroomCard';
+import Card from '@/components/Card/Card';
+import Link from 'next/link';
 
 export default function DMs({ params }) {
-  const s: string = 'hello';
+  const { authUser } = useAuth();
+  const [chats, setChats] = useState<KeyedChatRoom[]>();
+
+  useEffect(() => {
+    let tempChats: KeyedChatRoom[] = [];
+    const potentialRooms = query(
+      db.chatrooms,
+      where('participants', 'array-contains', authUser.uid)
+    );
+
+    const unsub = onSnapshot(potentialRooms, (snapshot) => {
+      snapshot.forEach((doc) => {
+        tempChats.push({ room: doc.data(), key: doc.id });
+      });
+      setChats(tempChats);
+    });
+    return () => unsub();
+  }, []);
+
   return (
     <>
-      <MessageModal userUID={s} />
+      <h1 className="text-4xl font-black ">
+        <b>Your DMs</b>
+      </h1>
+      <p className="mb-3 text-light-white-text">Strengthen your web</p>
+      <Card>
+        {chats?.map((keyRoom) => {
+          return (
+            <Link href={'/DM/' + keyRoom.key}>
+              <ChatroomCard key={keyRoom.key} room={keyRoom.room} />
+            </Link>
+          );
+        })}
+      </Card>
     </>
   );
-
-  //     const { currentUser, authUser } = useAuth();
-  //     const chatsRef = typeCollection<Message>(
-  //         collection(doc(db.messages, authUser.uid), authUser.uid)
-  //       ); // messages -> userID
-  //       useEffect(() => {
-  //         const museums = query(collectionGroup(firestore, 'messages'), where('type', '==', 'museum'));
-  //       }, []);
-  //   return (<div>
-  //   </div>);
-
-  // const { currentUser, authUser } = useAuth();
-  // let combinedID : string= "";
-  // let id = "FvfPPYhtKbalIp9eGO9OrMGRRNz1";
-  // if (authUser.uid === "FvfPPYhtKbalIp9eGO9OrMGRRNz1") id = "j6Qu8XsdUsg4NfnPFQ0IAgvhpX42";
-  // // Higher alphabetical order string always goes first in concatination
-  // // This ensures that twos users will share the same chatroom ID :)
-  // if (authUser.uid.localeCompare(id) == 1)
-  //     combinedID =id + authUser.uid ;
-  // else
-  //     combinedID = authUser.uid + id;
-  //  const senderRef = typeCollection<Message>(
-  //    collection(doc(db.messages, authUser.uid), id)
-  //  ); // messages -> userID -> participantID
-  // const receiverRef = typeCollection<Message>(
-  //    collection(doc(db.messages,id), authUser.uid)
-  //  ); // messages -> userID -> participantID
-  //   //getDoc(userRef).then((snapshot) => {console.log(snapshot.data())})
-  //   //await setDoc(userRef, {roomID: combinedID});
-  // const [message, setMessage] = useState<string>(''); // message to be sent
-  // const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]); // messages seen by both parties
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   if (message === '') return; //don't send empty messages
-  //   const newMessage: Message = {
-  //     content: message,
-  //     sender: {
-  //       name: currentUser.name,
-  //       profilePicture: currentUser.profilePicture,
-  //     },
-  //     time_stamp: Timestamp.now(),
-  //   };
-  //   await addDoc(receiverRef, newMessage); // add message to chatroom collection
-  //   await addDoc(senderRef, newMessage); // add message to chatroom collection
-  //   setMessage('');
-  // };
-  // useEffect(() => {
-  //   const getChatMessages = query(senderRef, orderBy('time_stamp'));
-  //   const unsubscribe = onSnapshot(getChatMessages, (snapshot) => {
-  //     let messages: ChatMessage[] = [];
-  //     snapshot.forEach((doc) => {
-  //       messages.push({ message: doc.data(), id: doc.id });
-  //     });
-  //     setChatMessages(messages);
-  //   });
-  //   return () => unsubscribe(); // removes listener
-  // }, []);
-  // return (
-  //   <div className="grid grid-cols-8 ">
-  //     <div className="col-span-4 col-start-3">
-  //       Your dms
-  //       <div>
-  //         {chatMessages.map((m) => {
-  //           return (
-  //             <div key={m.id}>
-  //               <MessageCard message={m.message} />
-  //             </div>
-  //           );
-  //         })}
-  //       </div>
-  //       <div>
-  //         <form onSubmit={handleSubmit}>
-  //           <div className="flex flex-row">
-  //             <div>
-  //               <input
-  //                 className="bg-purple-text-area"
-  //                 type="text"
-  //                 placeholder="Write your message..."
-  //                 value={message}
-  //                 onChange={(event) => setMessage(event.target.value)}
-  //               />
-  //             </div>
-  //             <div>
-  //               <button type="submit">
-  //                 <FaRegPaperPlane />
-  //               </button>
-  //             </div>
-  //           </div>
-  //         </form>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
