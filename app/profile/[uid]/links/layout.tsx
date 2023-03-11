1803b65b2f4e12ac49a4069b59081a3c6412f711
Koosha Gholipour <75464454+KooshaG@users.noreply.github.com>
2023-03-10 22:59:29 -0500

    Fixed layout

--- app/profile/[uid]/links/layout.tsx
+++ app/profile/[uid]/links/layout.tsx
@@ -1,16 +1,26 @@
-const linkLayout = ({ children }: { children: React.ReactNode }) => {
+import { db } from '@/config/firestore';
+import { getDoc, doc } from 'firebase/firestore';
+
+async function getUser(uid: string) {
+  const res = await getDoc(doc(db.users, uid));
+  return res.data();
+}
+
+export default async function linkLayout({ params, children }) {
+  const user = await getUser(params.uid);
+  if (!user || !user.name || !user.email)
+    return <h1 className="text-2xl font-extrabold">Invalid User.</h1>;
+
   return (
     <div className="container mx-auto text-white">
       <h1
         className="mb-3 text-left text-7xl font-extrabold"
         data-testid="title"
       >
-        Your Links
+        {`${user.name}'s Links`}
       </h1>
       {children}
       <div className="h-32"></div>
     </div>
   );
-};
-
-export default linkLayout;
+}
