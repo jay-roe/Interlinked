31e5b4ba4005924384cc8b729169749ca504e0b5
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-11 19:32:26 -0500

    added discloser to render time message was sent

--- components/DM/MessageCard.tsx
+++ components/DM/MessageCard.tsx
@@ -2,37 +2,51 @@ import Card from '../Card/Card';
 import { Message } from '@/types/Message';
 import { useAuth } from '@/contexts/AuthContext';
 import { User } from '@/types/User';
+import { Disclosure } from '@headlessui/react';
 
 const MessageCard = ({ message }: { message: Message }) => {
   const { currentUser } = useAuth();
 
   return (
-    <div
-      className={`my-3 flex gap-x-3 
+    <Disclosure>
+      <Disclosure.Button>
+        <div
+          className={`my-3 flex gap-x-3 
       ${
         message.sender.name == currentUser.name
           ? ' order-first justify-end '
           : ' order-last justify-start '
       }`}
-    >
-      <div
-        className={`${
-          message.sender.name == currentUser.name ? 'order-last ' : ''
-        }`}
-      >
-        <img
-          className="h-25 min-h-[2rem] w-8 min-w-[2rem] rounded-full p-2 md:h-12 md:w-12"
-          src={
-            message.sender?.profilePicture ||
-            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
-          }
-        ></img>
-      </div>
+        >
+          <div
+            className={`${
+              message.sender.name == currentUser.name ? 'order-last ' : ''
+            }`}
+          >
+            <img
+              className="h-25 min-h-[2rem] w-8 min-w-[2rem] rounded-full p-2 md:h-12 md:w-12"
+              src={
+                message.sender?.profilePicture ||
+                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
+              }
+            ></img>
+          </div>
 
-      <Card>
-        <div>{message.content}</div>
-      </Card>
-    </div>
+          <div className="rounded-xl bg-purple-message-area p-4">
+            <p>{message.content}</p>
+          </div>
+        </div>
+      </Disclosure.Button>
+      <Disclosure.Panel className="text-gray-500">
+        <p>
+          {' '}
+          {message.time_stamp.toDate().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+          })}
+        </p>
+      </Disclosure.Panel>
+    </Disclosure>
   );
 };
 
