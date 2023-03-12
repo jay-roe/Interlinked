24982a27b9869076b2196f86ed579b39e783f128
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-11 19:30:56 -0500

    sorted chats

--- app/DM/page.tsx
+++ app/DM/page.tsx
@@ -1,36 +1,46 @@
 'use client';
 import { KeyedChatRoom } from '@/types/Message';
 import { useAuth } from '@/contexts/AuthContext';
-import { where, onSnapshot, query } from 'firebase/firestore';
+import { where, onSnapshot, query, orderBy } from 'firebase/firestore';
 
 import { db } from '@/config/firestore';
 import { useEffect, useState } from 'react';
 import ChatroomCard from '@/components/DM/ChatroomCard';
 import Card from '@/components/Card/Card';
 import Link from 'next/link';
+import MessageModal from '@/components/MessageModal/MessageModal';
 
-export default function DMs({ params }) {
+export default function DMs() {
   const { authUser } = useAuth();
   const [chats, setChats] = useState<KeyedChatRoom[]>();
 
   useEffect(() => {
     let tempChats: KeyedChatRoom[] = [];
+    //get rooms that user is in
     const potentialRooms = query(
       db.chatrooms,
-      where('participants', 'array-contains', authUser.uid)
+      where('participants', 'array-contains', authUser.uid) //, orderBy("recentTimeStamp")  // create index in firebase
     );
 
     const unsub = onSnapshot(potentialRooms, (snapshot) => {
       snapshot.forEach((doc) => {
         tempChats.push({ room: doc.data(), key: doc.id });
       });
+
+      //sort chats by most recent
+      tempChats.sort((c1: KeyedChatRoom, c2: KeyedChatRoom) => {
+        return c1.room.recentTimeStamp.seconds > c2.room.recentTimeStamp.seconds
+          ? 1
+          : -1;
+      });
+
       setChats(tempChats);
     });
     return () => unsub();
   }, []);
 
   return (
-    <>
+    <div className="pb-12">
       <h1 className="text-4xl font-black ">
         <b>Your DMs</b>
       </h1>
@@ -44,6 +54,6 @@ export default function DMs({ params }) {
           );
         })}
       </Card>
-    </>
+    </div>
   );
 }
