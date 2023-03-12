e2525976791d459dc1fcc0cf50672d14e9c2b659
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-03-11 19:32:56 -0500

    added test id

--- components/DM/TimeDivider.tsx
+++ components/DM/TimeDivider.tsx
@@ -4,7 +4,7 @@ const TimeDivider = ({ time }: { time: Timestamp }) => {
   return (
     <div className="flex items-center space-x-5">
       <div className=" w-full border-t border-white "> </div>
-      <div className="no-wrap flex">
+      <div data-testid="time-divider-date" className=" no-wrap flex">
         {time.toDate().toLocaleString('en-US', {
           month: 'long',
           year: 'numeric',
