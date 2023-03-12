14b116291c300d4da1537216b55d953df3ef7e8c
Craig <craigk00@hotmail.com>
2023-03-11 19:36:43 -0500

    Update tests

--- app/tests/layout.test.tsx
+++ app/tests/layout.test.tsx
@@ -6,31 +6,31 @@ import RootLayout from '../layout';
 //   AuthProvider: ({ children }) => '<div>{children}</div>', // create mock function
 // }));
 
-jest.mock('contexts/AuthContext', () => ({
-  useAuth: jest.fn(),
-}));
+// jest.mock('contexts/AuthContext', () => ({
+//   useAuth: jest.fn(),
+// }));
 
-jest.mock('config/firebase', () => ({
-  storage: jest.fn(),
-}));
+// jest.mock('config/firebase', () => ({
+//   storage: jest.fn(),
+// }));
 
-jest.mock('firebase/storage', () => ({
-  getStorage: jest.fn(),
-  getDownloadURL: jest.fn(),
-  ref: jest.fn(),
-  uploadBytesResumable: jest.fn(),
-}));
+// jest.mock('firebase/storage', () => ({
+//   getStorage: jest.fn(),
+//   getDownloadURL: jest.fn(),
+//   ref: jest.fn(),
+//   uploadBytesResumable: jest.fn(),
+// }));
 
-jest.mock('firebase/firestore', () => ({
-  doc: jest.fn(),
-  setDoc: jest.fn(),
-  updateDoc: jest.fn(),
-  collection: () => {
-    return {
-      withConverter: jest.fn(),
-    };
-  },
-}));
+// jest.mock('firebase/firestore', () => ({
+//   doc: jest.fn(),
+//   setDoc: jest.fn(),
+//   updateDoc: jest.fn(),
+//   collection: () => {
+//     return {
+//       withConverter: jest.fn(),
+//     };
+//   },
+// }));
 
 it('renders the root', async () => {
   const { findByTestId } = render(<RootLayout> </RootLayout>);
