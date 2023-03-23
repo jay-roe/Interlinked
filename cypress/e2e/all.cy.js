e9fac62a2ebef96fa6d0c3c3f7bdbe74586e5266
Jay <roebuck.jayc+github@gmail.com>
2023-03-23 17:24:41 -0400

    added notification

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -2,6 +2,7 @@ import './auth.cy';
 import './edit-profile.cy';
 import './feed.cy';
 import './links.cy';
+import './notification.cy';
 import './dm.cy';
 import './navbar.cy';
 import './admin.cy';
