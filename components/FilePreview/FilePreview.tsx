275e94a348e76d9aed19b8d897709955ded64881
Adamo Orsini <adamo.orsini01@gmail.com>
2023-03-29 15:49:09 -0400

    Fixed tiny a tag issue

--- components/FilePreview/FilePreview.tsx
+++ components/FilePreview/FilePreview.tsx
@@ -9,7 +9,7 @@ export default function FilePreview({
 }) {
   return (
     <div className="rounded-md border-2 border-white bg-gray-600 p-2 ">
-      <a href={url} type={type} target="_blank" download>
+      <a href={url} type={type} target="_blank" rel="noreferrer" download>
         <p>{name}</p>
       </a>
     </div>
