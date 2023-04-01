c53444c1311074fea99c7daecfb1149c6696f0e1
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-03-31 22:01:54 -0400

    Fix home page tests

--- app/tests/page.test.tsx
+++ app/tests/page.test.tsx
@@ -1,13 +1,19 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { useAuth } from '@/contexts/AuthContext';
+import { useRouter } from 'next/navigation';
 import Home from '../page';
 
 jest.mock('contexts/AuthContext', () => ({
   useAuth: jest.fn(),
 }));
 
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+}));
+
 const mockedUseAuth = useAuth as jest.Mock<any>;
+const mockedUseRouter = useRouter as jest.Mock<any>;
 
 it('Head without user', async () => {
   mockedUseAuth.mockImplementation(() => {
@@ -29,4 +35,40 @@ it('Head with user', async () => {
   const { findByTestId } = render(<Home />);
   const welcome = await findByTestId('welcome-msg');
   expect(welcome).toBeInTheDocument();
+});
+
+it('redirects to locked page when account is locked', () => {
+  mockedUseAuth.mockReturnValue({
+    currentUser: { accountLocked: true },
+  });
+  mockedUseRouter.mockReturnValue({
+    push: jest.fn(),
+  });
+
+  render(<Home />);
+  expect(mockedUseRouter().push).toHaveBeenCalledWith('/locked');
+});
+
+it('redirects to locked page when account is timed out', () => {
+  mockedUseAuth.mockReturnValue({
+    currentUser: { accountTimeout: 1600 },
+  });
+  mockedUseRouter.mockReturnValue({
+    push: jest.fn(),
+  });
+
+  render(<Home />);
+  expect(mockedUseRouter().push).toHaveBeenCalledWith('/locked');
+});
+
+it('does not redirect when account is not locked or timed out', () => {
+  mockedUseAuth.mockReturnValue({
+    currentUser: {},
+  });
+  mockedUseRouter.mockReturnValue({
+    push: jest.fn(),
+  });
+
+  render(<Home />);
+  expect(mockedUseRouter().push).not.toHaveBeenCalled();
 });
