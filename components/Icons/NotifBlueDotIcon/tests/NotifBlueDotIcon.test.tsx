a8085452db6ff9d2caab272d01ab5c652e6d7ab7
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-03-31 22:03:31 -0400

    Add notif blue dot icon tests

---
+++ components/Icons/NotifBlueDotIcon/tests/NotifBlueDotIcon.test.tsx
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import NotifBlueDotIcon from '../NotifBlueDotIcon';
+
+describe('NotifBlueDotIcon', () => {
+  it('renders a blue dot when the notification is unread', () => {
+    const { container } = render(<NotifBlueDotIcon read={false} />);
+    const blueDot = container.querySelector('svg');
+    expect(blueDot).toBeInTheDocument();
+  });
+
+  it('does not render a blue dot when the notification is read', () => {
+    const { container } = render(<NotifBlueDotIcon read={true} />);
+    const blueDot = container.querySelector('svg');
+    expect(blueDot).not.toBeInTheDocument();
+  });
+});
