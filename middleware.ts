46b6774c4da37b30f1114d942bdad8b09e8c109e
Jay <roebuck.jayc+github@gmail.com>
2023-04-10 22:39:42 -0400

    add basic translations for landing page (still a LOT to do)

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import {createIntlMiddleware} from 'next-intl/server';
+ 
+export default createIntlMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'de'],
+ 
+  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
+  defaultLocale: 'en'
+});
+ 
