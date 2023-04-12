6f938301ab4e0eab487938a6074efacb3f8166a2
Jay <roebuck.jayc+github@gmail.com>
2023-04-11 20:52:42 -0400

    fixed build errors

--- app/[locale]/feed/layout.tsx
+++ app/[locale]/feed/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useTranslations } from 'next-intl';
 
 const feedLayout = ({ children }: { children: React.ReactNode }) => {
--- app/[locale]/job-feed/layout.tsx
+++ app/[locale]/job-feed/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useTranslations } from 'next-intl';
 
 const jobLayout = ({ children }: { children: React.ReactNode }) => {
