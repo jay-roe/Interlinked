73a267072284c586d0435c3e95bbd6aff786a48a
Julian Lussier <62126231+TheDude957@users.noreply.github.com>
2023-04-13 20:49:00 -0400

    added open graph tags

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,25 @@ export default function Head() {
         name="description"
         content="Connect with like minded tech enthusiasts for making friends or finding job oppurtunities! "
       />
+      <link rel="shortcut icon" href="/interlinked-logo.ico"></link>
       <link rel="icon" href="/interlinked-logo.ico" />
+
+      <meta property="og:title" content="Interlinked" />
+      <meta
+        property="og:description"
+        content="Connect with like minded tech enthusiasts for making friends or finding job oppurtunities!"
+      />
+      <meta name="image" property="og:image" content="/interlinked-logo.ico" />
+      <meta property="og:url" content="https://interlinked.live/" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="1200" />
+      <meta property="og:type" content="website" />
+      <meta name="theme-color" content="#130921" />
+
+      <meta property="twitter:title" content="Interlinked" />
+      <meta property="twitter:url" content="https://interlinked.live/" />
+      <meta property="twitter:image" content="/interlinked-logo.ico" />
+      <meta name="twitter:card" content="summary_large_image"></meta>
     </>
   );
 }
