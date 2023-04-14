99a9a8db4aa59a659273bb18b078b47d4fce4c9c
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-04-13 20:44:50 -0400

    Merge conflicts

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { useLocale, useTranslations } from 'next-intl';
 import { useEffect } from 'react';
 import { useAuth } from '@/contexts/AuthContext';
 import { FiEdit3, FiBriefcase, FiMessageSquare } from 'react-icons/fi';
+import Link from '@/components/Link/Link';
 
 const Home = () => {
   const router = useRouter();
@@ -15,13 +16,17 @@ const Home = () => {
   const links = [
     {
       href: '/feed',
-      label: 'Post to your feed',
+      label: t('post-to-your-feed'),
       icon: FiEdit3,
     },
-    { href: '/job-feed', label: 'Browse job opportunities', icon: FiBriefcase },
+    {
+      href: '/job-feed',
+      label: t('browse-job-opportunities'),
+      icon: FiBriefcase,
+    },
     {
       href: '/DM',
-      label: 'Discuss tech, jobs, or anything you desire',
+      label: t('discuss-tech-jobs-or-anything-you-desire'),
       icon: FiMessageSquare,
     },
   ];
@@ -43,10 +48,32 @@ const Home = () => {
       </h1>
       {/* Here goes the app's components */}
       {currentUser ? (
-        <p data-testid="welcome-msg" className="text-center text-2xl">
-          {t('welcome')}, {currentUser.name || currentUser.email}.
-          {t('get-you-interlinked')}.
-        </p>
+        <div>
+          <p data-testid="welcome-msg" className="text-center text-2xl">
+            {t('welcome')}, {currentUser.name || currentUser.email}.
+            {t('get-you-interlinked')}.
+          </p>
+          <ul className="mx-auto mt-20 max-w-3xl text-left text-4xl">
+            {links.map(({ href, label, icon: Icon }, index) => (
+              <li
+                key={`${href}${label}`}
+                className={`border-white ${
+                  index < links.length - 1 ? 'mb-2' : ''
+                }`}
+                style={{
+                  borderBottomWidth: index < links.length - 1 ? '2px' : 0,
+                }}
+              >
+                <Link href={href}>
+                  <div className="m-8 flex items-center">
+                    {Icon && <Icon className="mr-6 text-5xl" />}
+                    <span>{label}</span>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <>
           <p data-testid="base-msg" className="text-center text-2xl">
