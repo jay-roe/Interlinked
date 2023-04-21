6a87a218a8bf2a034cfb3d7c93fca43f963f0798
Adamo Orsini <adamo.orsini01@gmail.com>
2023-04-21 15:28:34 -0400

    Added DM privacy warning and DM redirect issue

--- app/[locale]/DM/[uid]/page.tsx
+++ app/[locale]/DM/[uid]/page.tsx
@@ -64,7 +64,7 @@ export default function ChatRoom({ params }) {
   }, [currentUser, router]);
 
   useEffect(() => {
-    if (!chatRoomRef || !authUser) return () => unsub();
+    if (!authUser) return;
 
     getDoc(chatRoomRef).then((room) => {
       if (room.data() === undefined) {
